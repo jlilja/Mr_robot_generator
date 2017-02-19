6a09420b1ee348ff0ebdf1a95a0c8f0580016f48
jcbl <jonas@lilja.io>
2017-02-19 16:35:11 +0100

    calls data from new src folder and added paths so script can be called from anywhere

--- app/app.sh
+++ app/app.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
-# Declare arrays
-terms=$(grep -i '"*",' terms.json | sed 's/",//g' | sed 's/"//g')
-video=$(grep -i '"*",' video.json | sed 's/",//g' | sed 's/"//g')
+# Get global paths
+SCRIPT=`realpath $0`
+SCRIPTPATH=`dirname $SCRIPT`
 
+# Declare arrays from json file
+# Filter out data with quotation ending with a comma
+# Extract string and put it in an array
+terms=$(grep -i '"*",' $SCRIPTPATH/../src/terms.json | sed 's/",//g' | sed 's/"//g')
+video=$(grep -i '"*",' $SCRIPTPATH/../src/video.json | sed 's/",//g' | sed 's/"//g')
+
+# Function to combine random index of array and get that value
 x()
 {
   # Assign argument to variable in order to access more than first item.
@@ -18,8 +25,14 @@ x()
 rs=$((1 + RANDOM % 2))
 re=$((RANDOM % 12))
 
+y=$(x ${terms})
+z=$(x ${video})
+
 # Put it all together
-result='eps'$rs'.'$re'_'$(x ${terms[@]})'.'$(x ${video})
+result='eps'$rs'.'$re'_'$y'.'$z
+
+# Clear terminal
+clear
 
 # echo result variable
 echo $result
